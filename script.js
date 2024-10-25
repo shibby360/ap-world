d3413cd44ff3da51d1a15d6d9b45183cac9d5bf9
Shivank Chhaya <shivank.chhaya@gmail.com>
2024-10-25 04:20:31 +0000

    added styles and iframe for amsco, and full notes

--- script.js
+++ script.js
@@ -1,7 +1,21 @@
 function amsco(unit_,lesson_) {
     let content = $('#amscoTemplate');
-    content.find('a').attr('href', baseUrl+'/unit'+unit_+'/lesson'+lesson_+'/Amsco_'+unit_+'.'+lesson_+'.pdf');
+    let amscoUrl = baseUrl+'/unit'+unit_+'/lesson'+lesson_+'/Amsco_'+unit_+'.'+lesson_+'.pdf'
+    content.find('a').attr('href', amscoUrl);
+    content.find('iframe').attr('src', amscoUrl);
     content.find('a').text("Amsco "+unit_+"."+lesson_+" Reading");
+    return content.html();
+}
+function notes(unit_, lesson_) {
+    let content = $('#notesTemplate');
+    let notesUrl = baseUrl+'/unit'+unit_+'/lesson'+lesson_+'/notes'+unit_+'.'+lesson_+'.pdf'
+    if(unit_ !== 1) {
+        content.find('a').attr('href', notesUrl);
+        content.find('a').text("Notes template for "+unit_+"."+lesson_);
+        content.find('iframe').attr('src', notesUrl);
+    } else {
+        content.find('a').text("No notes template available");
+    }
     return content.html();
 }
 if(unit !== null) {
@@ -9,10 +23,11 @@ if(unit !== null) {
     if(lesson === null) {
         
     } else {
-        let amscoDiv = $('<div id="amscoDiv">');
-        let contentDiv = $('<div id="amscoContentDiv">');
-        contentDiv.html(amsco(unit,lesson));
-        amscoDiv.append(contentDiv);
+        let amscoDiv = $('<div id="amscoDiv" class="content">');
+        amscoDiv.html(amsco(unit,lesson));
         $('#body').append(amscoDiv);
+        let notesDiv = $('<div id="notesDiv" class="content">');
+        notesDiv.html(notes(unit,lesson));
+        $('#body').append(notesDiv)
     }
 }
