6735eab1c19e480801631d4181027c14911d9196
Shivank Chhaya <shivank.chhaya@gmail.com>
2024-10-27 20:07:30 -0700

    added rest of unit 1 and all of units 2 and 3 eqs

--- eqs.js
+++ eqs.js
@@ -1,6 +1,26 @@
 eqs = {
     "unit1":{
         "lesson1":"How did developments in China and the rest of East Asia between c. 1200 and c.1450 reflect continuity, innovation, and diversity?",
-        "lesson2":"In the period c. 1200 to c. 1450, how did Islamic states arise, and how did major religions shape society?"
+        "lesson2":"In the period c. 1200 to c. 1450, how did Islamic states arise, and how did major religions shape society?",
+        "lesson3":"How did various beliefs and practices in South and Southeast Asia affect society and the development of states?",
+        "lesson4":"What states developed in the Americas, and how did they change over time?",
+        "lesson5":"How and why did states develop in Africa and change over time?",
+        "lesson6":"How did the beliefs and practices of the predominant religions, agricultural practices, and political decentralization affect European society from c. 1200 to c. 1450?",
+        "lesson7":"In what ways was the process of state-building in various parts of the world between c. 1200 and c. 1450 similar and different?"
+    },
+    "unit2":{
+        "lesson1":"What were the causes and effects of the growth of networks of exchange after 1200?",
+        "lesson2":"How and why did Eurasian empires grow over time, and how did their expansion influence trade and communication?",
+        "lesson3":"What were the causes and effects of the growth of networks of exchange after 1200, and how did environmental knowledge support that expansion?",
+        "lesson4":"What were the causes and effects of Trans-Saharan trade, and how did the growth of empires influence trade and communication?",
+        "lesson5":"What were the intellectual and cultural effects of the trade networks from c. 1200 to c. 1450?",
+        "lesson6":"What were some of the environmental effects of trade in Afro-Eurasia from c. 1200 to c. 1450?",
+        "lesson7":"What were the similarities and differences among the various networks of exchange in the period from c. 1200 to c. 1450?"
+    },
+    "unit3":{
+        "lesson1":"How did certain land-based empires develop and expand in the period from 1450-1750?",
+        "lesson2":"How did rulers in land-based empires legitimize and consolidate their power from 1450 to 1750?",
+        "lesson3":"How did different belief systems endure or change during the period frmo 1450-1750?",
+        "lesson4":"By what methods did empires increase their societal and cultural influence from c. 1450-c.1750?"
     }
 }
