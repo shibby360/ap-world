832a1fde08255ca3c616cb3ff608457f7d1a255a
Shivank Chhaya <shivank.chhaya@gmail.com>
2024-10-27 19:48:20 -0700

    added unit 2-4 lesson titles

--- lessonTitles.js
+++ lessonTitles.js
@@ -16,5 +16,21 @@ lessonTitles = {
         "lesson5":"Cultural Consequences of Connectivity",
         "lesson6":"Environmental Consequences of Connectivity",
         "lesson7":"Comparison of Economic Exchange"
+    },
+    "unit3":{
+        "lesson1":"Empires Expand",
+        "lesson2":"Empires: Administrations",
+        "lesson3":"Empires: Belief Systems",
+        "lesson4":"Comparison in Land-Based Empires"
+    },
+    "unit4":{
+        "lesson1":"Technological innovations from 1450 - 1750",
+        "lesson2":"Exploration: Causes and Events from 1450 to 1750",
+        "lesson3":"Columbian Exchange",
+        "lesson4":"Maritime Empires Established",
+        "lesson5":"Maritime Empires Maintained and Developed",
+        "lesson6":"International and External Challenges to State Power from 1450 to 1750",
+        "lesson7":"Changing Social Hierarchies from 1450 to 1750",
+        "lesson8":"Continuity and Change from 1450 to 1750"
     }
 }
