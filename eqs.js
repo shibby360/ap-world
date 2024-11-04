c30a1618daae74972cd9354b2ae043076c40b9d5
Shivank Chhaya <shivank.chhaya@gmail.com>
2024-11-03 21:49:13 -0800

    removed question marks

--- eqs.js
+++ eqs.js
@@ -1,48 +1,48 @@
 eqs = {
     "unit1":{
-        "topic1":"How did developments in China and the rest of East Asia between c. 1200 and c.1450 reflect continuity, innovation, and diversity?",
-        "topic2":"In the period c. 1200 to c. 1450, how did Islamic states arise, and how did major religions shape society?",
-        "topic3":"How did various beliefs and practices in South and Southeast Asia affect society and the development of states?",
-        "topic4":"What states developed in the Americas, and how did they change over time?",
-        "topic5":"How and why did states develop in Africa and change over time?",
-        "topic6":"How did the beliefs and practices of the predominant religions, agricultural practices, and political decentralization affect European society from c. 1200 to c. 1450?",
-        "topic7":"In what ways was the process of state-building in various parts of the world between c. 1200 and c. 1450 similar and different?"
+        "topic1":"How did developments in China and the rest of East Asia between c. 1200 and c.1450 reflect continuity, innovation, and diversity",
+        "topic2":"In the period c. 1200 to c. 1450, how did Islamic states arise, and how did major religions shape society",
+        "topic3":"How did various beliefs and practices in South and Southeast Asia affect society and the development of states",
+        "topic4":"What states developed in the Americas, and how did they change over time",
+        "topic5":"How and why did states develop in Africa and change over time",
+        "topic6":"How did the beliefs and practices of the predominant religions, agricultural practices, and political decentralization affect European society from c. 1200 to c. 1450",
+        "topic7":"In what ways was the process of state-building in various parts of the world between c. 1200 and c. 1450 similar and different"
     },
     "unit2":{
-        "topic1":"What were the causes and effects of the growth of networks of exchange after 1200?",
-        "topic2":"How and why did Eurasian empires grow over time, and how did their expansion influence trade and communication?",
-        "topic3":"What were the causes and effects of the growth of networks of exchange after 1200, and how did environmental knowledge support that expansion?",
-        "topic4":"What were the causes and effects of Trans-Saharan trade, and how did the growth of empires influence trade and communication?",
-        "topic5":"What were the intellectual and cultural effects of the trade networks from c. 1200 to c. 1450?",
-        "topic6":"What were some of the environmental effects of trade in Afro-Eurasia from c. 1200 to c. 1450?",
-        "topic7":"What were the similarities and differences among the various networks of exchange in the period from c. 1200 to c. 1450?"
+        "topic1":"What were the causes and effects of the growth of networks of exchange after 1200",
+        "topic2":"How and why did Eurasian empires grow over time, and how did their expansion influence trade and communication",
+        "topic3":"What were the causes and effects of the growth of networks of exchange after 1200, and how did environmental knowledge support that expansion",
+        "topic4":"What were the causes and effects of Trans-Saharan trade, and how did the growth of empires influence trade and communication",
+        "topic5":"What were the intellectual and cultural effects of the trade networks from c. 1200 to c. 1450",
+        "topic6":"What were some of the environmental effects of trade in Afro-Eurasia from c. 1200 to c. 1450",
+        "topic7":"What were the similarities and differences among the various networks of exchange in the period from c. 1200 to c. 1450"
     },
     "unit3":{
-        "topic1":"How did certain land-based empires develop and expand in the period from 1450-1750?",
-        "topic2":"How did rulers in land-based empires legitimize and consolidate their power from 1450 to 1750?",
-        "topic3":"How did different belief systems endure or change during the period from 1450-1750?",
-        "topic4":"By what methods did empires increase their societal and cultural influence from c. 1450-c.1750?"
+        "topic1":"How did certain land-based empires develop and expand in the period from 1450-1750",
+        "topic2":"How did rulers in land-based empires legitimize and consolidate their power from 1450 to 1750",
+        "topic3":"How did different belief systems endure or change during the period from 1450-1750",
+        "topic4":"By what methods did empires increase their societal and cultural influence from c. 1450-c.1750"
     },
     "unit4":{
-        "topic1":"How did cross-cultural interactions spread technology and facilitate changes in trade and travel from 1450 to 1750?",
-        "topic2":"What were the causes and effects of the state-sponsored expansion of maritime exploration?",
-        "topic3":"What were the causes of the Columbian Exchange and its effects on the Eastern and Western Hemispheres?",
-        "topic4":"How were the empires of European states established between 1450 to 1750, and what economic and labor systems fueled them?",
-        "topic5":"What econmic stategies did maritime empires use to increase their power, and how did the developing empires affect political, econmic, religious, and cultural dynamics?",
-        "topic6":"How did the development of state power result in external and internal challenges in the period between 1450 and 1750?",
-        "topic7":"How were social categories, roles, and practices maintained or changed from 1450 to 1750?",
-        "topic8":"How did economic developments from 1450 to 1750 affect social structures over time?"
+        "topic1":"How did cross-cultural interactions spread technology and facilitate changes in trade and travel from 1450 to 1750",
+        "topic2":"What were the causes and effects of the state-sponsored expansion of maritime exploration",
+        "topic3":"What were the causes of the Columbian Exchange and its effects on the Eastern and Western Hemispheres",
+        "topic4":"How were the empires of European states established between 1450 to 1750, and what economic and labor systems fueled them",
+        "topic5":"What econmic stategies did maritime empires use to increase their power, and how did the developing empires affect political, econmic, religious, and cultural dynamics",
+        "topic6":"How did the development of state power result in external and internal challenges in the period between 1450 and 1750",
+        "topic7":"How were social categories, roles, and practices maintained or changed from 1450 to 1750",
+        "topic8":"How did economic developments from 1450 to 1750 affect social structures over time"
     },
     "unit5":{
-        "topic1":"How did the Enlightenment shapen the intellectual and ideological thinking that affected reform and revolution after 1750?",
-        "topic2":"What were the causes and effects of the various revolutions in the period from 1750 to 1900, including influences of the Enlightenment and emerging nationalism?",
-        "topic3":"What factors contributed to and characterized industrialization in the period from 1750 to 1900?",
-        "topic4":"How did different types and locations of production develop and change over time?",
-        "topic5":"How did technology shape economic production during the period from 1750 to 1900?",
-        "topic6":"What economic strategies did different states and empires adopt, and what were the causes and effects of those strategies?",
-        "topic7":"How did the development of economic systems, ideologies, and institutions contribute to change between 1750 and 1900?",
-        "topic8":"What conditions led to calls for change in industrial societies, and what were the effects of those efforts?",
-        "topic9":"How did industrialization cause change in existing social hierarchies and standards of living?",
-        "topic10":"How did the Industrial Revolution demonstrate both continuity and change?"
+        "topic1":"How did the Enlightenment shapen the intellectual and ideological thinking that affected reform and revolution after 1750",
+        "topic2":"What were the causes and effects of the various revolutions in the period from 1750 to 1900, including influences of the Enlightenment and emerging nationalism",
+        "topic3":"What factors contributed to and characterized industrialization in the period from 1750 to 1900",
+        "topic4":"How did different types and locations of production develop and change over time",
+        "topic5":"How did technology shape economic production during the period from 1750 to 1900",
+        "topic6":"What economic strategies did different states and empires adopt, and what were the causes and effects of those strategies",
+        "topic7":"How did the development of economic systems, ideologies, and institutions contribute to change between 1750 and 1900",
+        "topic8":"What conditions led to calls for change in industrial societies, and what were the effects of those efforts",
+        "topic9":"How did industrialization cause change in existing social hierarchies and standards of living",
+        "topic10":"How did the Industrial Revolution demonstrate both continuity and change"
     }
 }
