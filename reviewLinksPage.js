2ccae06538b620607146c15c9aee2f7a45c1ab7d
Shivank Chhaya <shivank.chhaya@gmail.com>
2024-11-11 20:28:55 -0800

    removed iframes from review-linkspage

--- reviewLinksPage.js
+++ reviewLinksPage.js
@@ -12,9 +12,7 @@ function putLinks(text) {
         let splitLink = link.split('|');
         let lnkRef = splitLink[1];
         let lnkTxt = splitLink[0] == "" ? splitLink[1] : splitLink[0];
-        let linkDiv = $(`<div>
-            <div><a style="margin:0;" href="${lnkRef}" target="_blank">${lnkTxt}</a><iframe src="${lnkRef}"></iframe></div>
-            </div>`)
+        let linkDiv = $(`<div><a style="margin:0;" href="${lnkRef}" target="_blank">${lnkTxt}</a></div>`)
         $('#links').append(linkDiv);
     }
 }
