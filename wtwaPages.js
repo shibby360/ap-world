db43c535a1484fefe2d016fd88944100c2546d28
Shivank Chhaya <shivank.chhaya@gmail.com>
2024-11-12 21:25:10 -0800

    added wtwa page data

--- wtwaPages.js
+++ wtwaPages.js
@@ -1,17 +1,23 @@
 wtwaPages = {
     "unit1":[
-        
+        "P. 3-38",
+        "Islam 57-70, 71-75, 118-122",
+        "East Asia 76-86, 126-133",
+        "Europe 87-95, 133-137",
+        "South Asia 122-126",
+        "Africa 137-140",
+        "Americas 141-145"
     ],
     "unit2":[
-
+        "Chapters 3 and 4"
     ],
     "unit3":[
-
+        "Chapters 4 and 5"
     ],
     "unit4":[
-
+        "Chapters 6 and 7"
     ],
     "unit5":[
-
+        
     ]
 }
